05562c88f83b177ab4f6628ddd7abb2d30536367
pooja <poojamarudhu@gmail.com>
2026-05-08 21:01:18 +0530

    home

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+// Home.js
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Code Snippet App</h1>
+      <p>Store and manage your code snippets easily.</p>
+    </div>
+  );
+}
+
+export default Home;
